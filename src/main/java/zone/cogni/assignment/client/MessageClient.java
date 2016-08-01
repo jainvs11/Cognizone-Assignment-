8b31caeb4aa1db35acb86a14ad73c09490e0a7be
Vishal Jain <vishal6jain@gmail.com>
2016-08-01 21:42:57 +0530

    Last Commit

--- src/main/java/zone/cogni/assignment/client/MessageClient.java
+++ src/main/java/zone/cogni/assignment/client/MessageClient.java
@@ -3,15 +3,9 @@ package zone.cogni.assignment.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import zone.cogni.assignment.controller.MessageController;
-import zone.cogni.assignment.dao.MessageDAO;
-import zone.cogni.assignment.service.MessageService;
-
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "zone.cogni.assignment")
 public class MessageClient {
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(
-				new Object[] { MessageController.class, MessageClient.class, MessageService.class, MessageDAO.class },
-				args);
+		SpringApplication.run(MessageClient.class, args);
 	}
 }
